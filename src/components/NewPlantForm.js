a74d29ee9e590b6c75ba81e32ee8c00efce35e69
Jessica <jess.newman19@gmail.com>
2022-05-04 11:07:59 -0400

    all four deliverables done

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({updatePlantList}) {
+  const [formItem, setFormItem] = useState({ 
+    name: "", 
+    image: "",
+    price: 0,
+  })
+
+  function handleChange(e) { 
+    const {name, value} = e.target
+    setFormItem(previousState => ({...previousState, [name]: value}))
+  }
+
+  function handleSubmit (e) { 
+    e.preventDefault()
+    fetch("http://localhost:6001/plants", { 
+      method: "POST", 
+      headers: { 
+        "Content-Type": "application/json",
+      }, 
+      body: JSON.stringify(formItem)
+    })
+    .then(response => response.json())
+    .then(newData => updatePlantList(newData))
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={formItem.name} onChange={handleChange}/>
+        <input type="text" name="image" placeholder="Image URL" value={formItem.image} onChange={handleChange}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formItem.price} onChange={handleChange}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [query, setQuery] = useState("")
+
+  useEffect(() => { 
+    fetch("http://localhost:6001/plants")
+    .then(response=> response.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  function updatePlantList(newPlant) {
+    setPlants([...plants, newPlant])
+}
+
+  function changeSearch (searchedItem) {
+    setQuery(searchedItem)
+  }
+  
+  const filteredPlants = !query ? plants : plants.filter(plant => plant.name.includes(query))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm updatePlantList={updatePlantList}/>
+      <Search plants={plants} query={query} changeSearch={changeSearch}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
